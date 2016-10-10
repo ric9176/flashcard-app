4ba99648f481a70e879fc8c0702b034d291b50b0
Richard Moss <richard@englishtutorslive.com>
2016-10-10 23:54:07 +0100

    hooks up state and action creators with sidebar

--- public/bundle.js
+++ public/bundle.js
@@ -1,5 +1,15 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
-'use strict';
+"use strict";
+
+var addDeck = function addDeck(name) {
+  return { type: "ADD_DECK", data: name };
+};
+var showAddDeck = function showAddDeck() {
+  return { type: "SHOW_ADD_DECK" };
+};
+var hideAddDeck = function hideAddDeck() {
+  return { type: "HIDE_ADD_DECK" };
+};
 
 var cards = function cards(state, action) {
   switch (action.type) {
@@ -16,53 +26,95 @@ var cards = function cards(state, action) {
   }
 };
 
+var decks = function decks(state, action) {
+  switch (action.type) {
+    case 'ADD_DECK':
+      var newDeck = { name: action.data, id: +new Date() };
+      return state.concat([newDeck]);
+    default:
+      return state || [];
+  }
+};
+
+var addingDeck = function addingDeck(state, action) {
+  switch (action.type) {
+    case 'SHOW_ADD_DECK':
+      return true;
+    case 'HIDE_ADD_DECK':
+      return false;
+    default:
+      return state || false;
+
+  }
+};
+
 var store = Redux.createStore(Redux.combineReducers({
-  cards: cards
+  cards: cards,
+  decks: decks,
+  addingDeck: addingDeck
 }));
 
 var App = function App(props) {
   return React.createElement(
-    'div',
-    { className: 'app' },
+    "div",
+    { className: "app" },
     props.children
   );
 };
 
 var Sidebar = React.createClass({
-  displayName: 'Sidebar',
+  displayName: "Sidebar",
   render: function render() {
     var props = this.props;
 
     return React.createElement(
-      'div',
-      { className: 'sidebar' },
+      "div",
+      { className: "sidebar" },
       React.createElement(
-        'h2',
+        "h2",
         null,
-        'All decks:'
+        "All decks:"
       ),
       React.createElement(
-        'ul',
+        "ul",
         null,
         props.decks.map(function (deck, i) {
           return React.createElement(
-            'li',
+            "li",
             { key: i },
-            ' ',
+            " ",
             deck.name,
-            ' '
+            " "
           );
         })
       ),
-      props.addingDeck && React.createElement('input', { ref: 'add' })
+      props.addingDeck && React.createElement("input", { ref: "add" })
     );
   }
 });
 
-ReactDOM.render(React.createElement(
-  App,
-  null,
-  React.createElement(Sidebar, { decks: [{ name: 'Deck 1' }], addingDeck: false })
-), document.getElementById('root'));
+function run() {
+  var state = store.getState();
+  console.log(state);
+  ReactDOM.render(React.createElement(
+    App,
+    null,
+    React.createElement(Sidebar, { decks: state.decks, addingDeck: state.addingDeck })
+  ), document.getElementById('root'));
+}
+
+run();
+
+store.subscribe(run);
+
+window.show = function () {
+  return store.dispatch(showAddDeck());
+};
+window.hide = function () {
+  return store.dispatch(hideAddDeck());
+};
+window.add = function () {
+  return store.dispatch(addDeck(new Date().toString()));
+};
 
 },{}]},{},[1]);
--- src/app.js
+++ src/app.js
@@ -1,4 +1,6 @@
-
+const addDeck = name => ({ type:"ADD_DECK", data:name });
+const showAddDeck = () => ({ type: "SHOW_ADD_DECK"});
+const hideAddDeck = () => ({ type: "HIDE_ADD_DECK"});
 
 const cards = (state, action) => {
   switch (action.type) {
@@ -15,9 +17,29 @@ const cards = (state, action) => {
     }
 };
 
+const decks = (state, action) => {
+  switch (action.type) {
+    case 'ADD_DECK':
+      let newDeck = { name: action.data,  id: +new Date };
+      return state.concat([newDeck]);
+    default:
+      return state || [];
+  }
+};
+
+const addingDeck = (state, action) => {
+  switch (action.type) {
+    case 'SHOW_ADD_DECK': return true;
+    case 'HIDE_ADD_DECK': return false;
+    default: return state || false;
+
+  }
+};
 
 const store = Redux.createStore(Redux.combineReducers({
-  cards
+  cards,
+  decks,
+  addingDeck
 }));
 
 const App = (props) => {
@@ -42,6 +64,18 @@ const Sidebar = React.createClass({
   }
 });
 
+function run() {
+  let state = store.getState();
+  console.log(state);
 ReactDOM.render(<App>
-  <Sidebar decks={[{ name: 'Deck 1'}]} addingDeck={false} />
+  <Sidebar decks={state.decks} addingDeck={state.addingDeck} />
  </App>, document.getElementById('root'));
+ }
+
+ run();
+
+ store.subscribe(run);
+
+ window.show = () => store.dispatch(showAddDeck());
+ window.hide = () => store.dispatch(hideAddDeck());
+ window.add = () => store.dispatch(addDeck(new Date().toString()));
